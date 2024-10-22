9bef992e3f80dc5db7609b55dba945664662b0c7
Quang Nguyen <32150689+quangnguyenbn99@users.noreply.github.com>
2024-10-22 12:22:23 +0900

    Update run_class.sh

--- run_class.sh
+++ run_class.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 echo "Please enter the GPU address"
 read GPU
-echo "Sellected GPU: $GPU"
+echo "Selected GPU: $GPU"
 
 #train
-#CUDA_VISIBLE_DEVICES=$GPU python train.py --model_nam model9 --batch_size 75 --n_epochs 30 --task novel_comp --dropout 0.9 --result_name model9_classifcation_mean_aggdrop0.9
+CUDA_VISIBLE_DEVICES=$GPU python train.py --model_nam model9 --batch_size 75 --n_epochs 30 --task novel_comp --dropout 0.9 --result_name model9_classifcation_mean_aggdrop
